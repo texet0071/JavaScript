470a58646dd06070e9386ba453c8e9ba8966363a
Maxim Chuagev <texet0071@gmail.com>
2020-12-19 00:24:54 +0500

    new

---
+++ new test/js/script.js
@@ -0,0 +1,29 @@
+var products = [    {name: " Grapefruit", calories: 170, color: "red", sold: 8200 },
+                    {name: " Orange", calories: 160, color: "orange", sold: 12101 },
+                    {name: " Cola", calories: 210, color: "caramel", sold: 25412 },
+                    {name: " Diet Cola", calories: 0, color: "caramel", sold: 43922 },
+                    {name: " Lemon", calories: 200, color: "clear", sold: 14983 },
+                    {name: " Raspberry", calories: 180, color: "pink", sold: 9427 },
+                    {name: " Root Beer", calories: 200, color: "caramel", sold: 9909 },
+                    {name: " Water", calories: 0, color: "clear", sold: 62123 }
+            ];
+
+function compareCalories(colaA, colaB) {
+  return colaA.calories - colaB.calories;
+}
+
+function printProduct(products) {
+    for (var i = 0; i < products.length; i++){
+        console.log("Name: " + products[i].name + 
+                    ", Calories: " + products[i].calories +
+                    ", Color: " + products[i].color +
+                    ", sold " + products[i].sold);
+    }
+}
+
+
+products.sort(compareCalories);
+console.log("Product sorted by calories");
+printProduct(products);
+
+
